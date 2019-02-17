1649dc1cd5abcd0319ebd855887516b81b5d34e9
Ritesh Chaman <rchaman@HW14051.local>
2019-02-16 18:03:20 -0800

    with two nearest neighbors

--- my_kdtree.py
+++ my_kdtree.py
@@ -1,4 +1,5 @@
 import sys
+import re
 import kdtree
 
 data = []
@@ -7,6 +8,33 @@ query = []
 query_int = []
 tree_col = []
 tree_col_int = []
+thisdict ={}
+
+##Adding all three elements in dictionary##
+def datadict(x):
+	arg1 = (x[0]+","+x[1])
+	arg2=x[2]
+	thisdict[arg1]=arg2
+
+##Get the value for key value from thisdict
+def neigh(value1,value2):
+	num1 = re.sub(r'[\]\[]*', "",str(value1)).split(",")
+	num2 = re.sub(r'[\]\[]*', "",str(value2)).split(",")
+	num1[0]=format(float(num1[0]), '.2f')   ##Setting precision to match dictionary key 
+	num1[1]=format(float(num1[1]), '.2f')
+	num2[0]=format(float(num2[0]), '.2f')
+	num2[1]=format(float(num2[1]), '.2f')
+	q1 = (num1[0]+","+num1[1]) 		##Creating key string
+	q2 = (num2[0]+","+num2[1])
+	neigh_val1=thisdict[q1]			##Fetching value for key created
+	neigh_val2=thisdict[q2]
+	decision(neigh_val1,neigh_val2)
+
+def decision(x,y)
+	if "x"=="y":
+		return neigh_val1
+
+####Reading the data from input file#####
 for line in sys.stdin:
 	if line.strip()[0] == '#':
 		continue
@@ -15,10 +43,10 @@ for line in sys.stdin:
 	else:
 		col_line=line.rstrip().split(" ")
 		if len(col_line) == 3:
-			data.append(col_line)
+			datadict(col_line)
 			tree_col.append(col_line[:2])
 		elif len(col_line) == 2:
-			query.append(col_line)	
+			query.append(col_line)
 
 ##Converting all string lists to integer values##
 for x in query:
@@ -27,20 +55,6 @@ for x in query:
 for x in tree_col:
 	tree_col_int.append(list(map(float,x)))
 
-###Class##
-class Item(object):
-    def __init__(self, x, y, data):
-        self.coords = (x, y)
-        self.data = data
-
-    def __len__(self):
-        return len(self.coords)
-
-    def __getitem__(self, i):
-        return self.coords[i]
-
-    def __repr__(self):
-        return 'Item({}, {}, {})'.format(self.coords[0], self.coords[1], self.data)
 	
 # Creating tree from coordinates
 emptyTree = kdtree.create(dimensions=2)
@@ -50,5 +64,5 @@ tree = kdtree.create(tree_col_int)
 for out in query_int:
 	x = out[0]
 	y = out[1]
-	output=tree.search_nn((x,y))
-	print (output)
+	neighbors=(tree.search_nn((x,y)))
+	neigh(neighbors[0],neighbors[1])

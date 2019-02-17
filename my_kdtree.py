a3955165af593ace57a83f9b6ca2f5175ce5484e
Ritesh Chaman <rchaman@HW14051.local>
2019-02-17 01:34:02 -0800

    Final kdtree with error handling

--- my_kdtree.py
+++ my_kdtree.py
@@ -10,7 +10,7 @@ tree_col_int = []
 thisdict = {}
 t_dict={}
 
-
+#Updating the Dictionary with new values
 def datadict(x):
         arg1 = (x[0]+","+x[1])
         arg2=x[2]
@@ -33,7 +33,7 @@ def neigh(value1,value2):
 	final_val1 = decision(neigh_val1,neigh_val2)
 	return (final_val1)
 	
-
+#Predicting the Class
 def decision(x,y):
 	if "x"=="y":
 		return (x)  
@@ -41,27 +41,11 @@ def decision(x,y):
 		return (x) ##Taking closest neighbor as high priority
 
 # Creating tree from coordinates
-def create_tree (k,list):
+def create_tree (list):
 	emptyTree = kdtree.create(dimensions=2)
 	f_tree = kdtree.create(list)
 	f_tree = f_tree.rebalance()
 	return (f_tree)
-
-####Reading the data from input file#####
-for line in sys.stdin:
-	if line.strip()[0] == '#':
-		continue
-	elif "#" in line:
-		data.append(line.rstrip().split('#')[0])
-	else:
-		col_line=line.rstrip().split(" ")
-		if len(col_line) == 3:
-			thisdict=datadict(col_line)
-			tree_col.append(col_line[:2])
-		elif len(col_line) == 2:
-			query.append(col_line)
-
-
 
 ##Converting all string lists to integer values##
 def convertion_query(q):
@@ -70,33 +54,61 @@ def convertion_query(q):
 		q_int.append(list(map(float,x)))
 	return (q_int)
 
+#Converting the list to the decimal format 
 def convertion_tree_list (t):
 	t_col_int=[]
 	for x in t:
 		t_col_int.append(list(map(float,x)))
 	return (t_col_int)
 
+####Reading the data from input file#####
+if __name__ == "__main__":
+	for line in sys.stdin:
+		col_line=line.rstrip().split(" ")	
+		try:
+			if len(col_line) == 3 and type(float(col_line[0])) == float and type(float(col_line[1])) == float and type(col_line[2]) == str:
+				thisdict=datadict(col_line)
+				tree_col.append(col_line[:2])
+			elif len(col_line) == 2 and type(float(col_line[0])) == float and type(float(col_line[1])) == float:
+				query.append(col_line)                	
+			else:
+                		continue
+		except:
+			continue
+
 ##Invoking convertions
 query_int=convertion_query(query)
 tree_col_int=convertion_tree_list(tree_col)
 
 ##Creating tree from elements in list 
-tree=create_tree(2,tree_col_int)
+tree=create_tree(tree_col_int)
 
 	
 #Fetching query and finding the nearest neighbour
-for out in query_int:
-	x = out[0]
-	y = out[1]
-	neighbors=(tree.search_nn((x,y)))
-	new_value=neigh(neighbors[0],neighbors[1])
-	print (x,y,new_value)	
-	x = format(float(out[0]), '.2f')
-	y = format(float(out[1]), '.2f')
-	str1=' '.join((str(x),str(y),new_value))
-	str2=str1.split(" ")[:3]
-	thisdict=datadict(str2) 						##Updating the dictionary with the new value
-	str3=str1.split(" ")[:2]
-	tree_col.append(str3)					##Updating the list with which tree is created
-	tree_col_int=convertion_tree_list(tree_col)          	##Reconverting list to float values for error minimization
-	tree=create_tree(2,tree_col_int) 			##Recreating tree before every new search
+try:
+	for out in query_int:
+		x = out[0]
+		y = out[1]
+		neighbors=(tree.search_nn((x,y)))
+		new_value=neigh(neighbors[0],neighbors[1])
+		print (x,y,new_value)	
+		x = format(float(out[0]), '.2f')
+		y = format(float(out[1]), '.2f')
+		str1=' '.join((str(x),str(y),new_value))
+		str2=str1.split(" ")[:3]
+		thisdict=datadict(str2) 						##Updating the dictionary with the new value
+		str3=str1.split(" ")[:2]
+		tree_col.append(str3)					##Updating the list with which tree is created
+		tree_col_int=convertion_tree_list(tree_col)          	##Reconverting list to float values for error minimization
+		tree=create_tree(tree_col_int)			##Recreating tree before every new search
+except IOError:
+	print('An Error occured trying to read the file')
+except ValueError:
+        print('Non-Numeric data found in the file')
+except ImportError:
+        print('No module found')
+except EOFError:
+        print('An Error occured while reading a file')
+except:
+        print('An Error occured')
+
